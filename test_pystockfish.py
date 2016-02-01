36fe03b7d7958c64becb76536405b5e118389364
Ilya Zhelyabuzhsky <zhelyabuzhsky@gmail.com>
2016-02-01 19:14:09 +0500

    Fixed test

--- test_pystockfish.py
+++ test_pystockfish.py
@@ -3,12 +3,13 @@ from pystockfish import *
 
 
 class TestStockfish(unittest.TestCase):
+
     def setUp(self):
         self.stockfish = Stockfish()
 
     def testRun(self):
         best_move = self.stockfish.get_best_move()
-        self.assertEqual(best_move, 'e2e4')
+        self.assertIn(best_move, ('e2e4', 'g1f3',))
 
         self.stockfish.set_position(['e2e4', 'e7e6'])
         best_move = self.stockfish.get_best_move()
