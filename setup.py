4a59c772347f192e54c7f4ceb23b7eb5aefa4297
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-08-06 19:34:11 +0500

    Removed utf-8 comment

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
 from setuptools import setup, find_packages
 import os
 
-package_version = os.environ.get('TRAVIS_TAG') or '0.dev0'
+package_version = os.environ.get("TRAVIS_TAG") or "0.dev0"
 
-with open('README.md', 'r') as readme:
+with open("README.md", "r") as readme:
     long_description = readme.read()
 
 setup(
     name="stockfish",
-    author='Ilya Zhelyabuzhsky',
+    author="Ilya Zhelyabuzhsky",
     author_email="zhelyabuzhsky@icloud.com",
     version=package_version,
     license="GPLv3",
     keywords="chess stockfish",
-    url='https://github.com/zhelyabuzhsky/stockfish',
+    url="https://github.com/zhelyabuzhsky/stockfish",
     description="Wraps the open-source Stockfish chess engine for easy integration into python.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=find_packages(include=['stockfish', 'stockfish.*']),
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["stockfish", "stockfish.*"]),
     install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: Unix",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Development Status :: 5 - Production/Stable",
         "Topic :: Games/Entertainment :: Board Games",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-    ])
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
--- tests/stockfish/test_models.py
+++ tests/stockfish/test_models.py
@@ -6,34 +6,35 @@ from stockfish import Stockfish
 
 
 class TestStockfish(unittest.TestCase):
-
     def setUp(self):
         self.stockfish = Stockfish()
 
     def test_get_best_move(self):
         best_move = self.stockfish.get_best_move()
-        self.assertIn(best_move, ('e2e4', 'g1f3', 'b1c3',))
+        self.assertIn(best_move, ("e2e4", "g1f3", "b1c3"))
 
-        self.stockfish.set_position(['e2e4', 'e7e6'])
+        self.stockfish.set_position(["e2e4", "e7e6"])
         best_move = self.stockfish.get_best_move()
-        self.assertIn(best_move, ('d2d4', 'g1f3',))
+        self.assertIn(best_move, ("d2d4", "g1f3"))
 
         # mate
-        self.stockfish.set_position(['f2f3', 'e7e5', 'g2g4', 'd8h4'])
+        self.stockfish.set_position(["f2f3", "e7e5", "g2g4", "d8h4"])
         self.assertFalse(self.stockfish.get_best_move())
 
     def test_set_fen_position(self):
-        self.stockfish.set_fen_position("7r/1pr1kppb/2n1p2p/2NpP2P/5PP1/1P6/P6K/R1R2B2 w - - 1 27")
-        self.assertTrue(self.stockfish.is_move_correct('f4f5'))
-        self.assertFalse(self.stockfish.is_move_correct('a1c1'))
+        self.stockfish.set_fen_position(
+            "7r/1pr1kppb/2n1p2p/2NpP2P/5PP1/1P6/P6K/R1R2B2 w - - 1 27"
+        )
+        self.assertTrue(self.stockfish.is_move_correct("f4f5"))
+        self.assertFalse(self.stockfish.is_move_correct("a1c1"))
 
     def test_is_move_correct(self):
-        self.assertFalse(self.stockfish.is_move_correct('e2e1'))
-        self.assertTrue(self.stockfish.is_move_correct('a2a3'))
-        self.stockfish.set_position(['e2e4', 'e7e6'])
-        self.assertFalse(self.stockfish.is_move_correct('e2e1'))
-        self.assertTrue(self.stockfish.is_move_correct('a2a3'))
+        self.assertFalse(self.stockfish.is_move_correct("e2e1"))
+        self.assertTrue(self.stockfish.is_move_correct("a2a3"))
+        self.stockfish.set_position(["e2e4", "e7e6"])
+        self.assertFalse(self.stockfish.is_move_correct("e2e1"))
+        self.assertTrue(self.stockfish.is_move_correct("a2a3"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
