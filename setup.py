19d1528275dcb7ac664d8cd4e9a3b326ef3a279f
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2019-10-09 14:52:57 +0500

    3.2.0 version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="stockfish",
     author="Ilya Zhelyabuzhsky",
     author_email="zhelyabuzhsky@icloud.com",
-    version="3.1.1",
+    version="3.2.0",
     license="GPLv3",
     keywords="chess stockfish",
     python_requires=">=3.7",
