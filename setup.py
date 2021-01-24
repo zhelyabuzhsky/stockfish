7f2c12678a4145f63eccaff8f98d3e2c214a44bb
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2021-01-24 19:58:41 +0500

    Added get_stockfish_major_version function

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="stockfish",
     author="Ilya Zhelyabuzhsky",
     author_email="zhelyabuzhsky@icloud.com",
-    version="3.11.0",
+    version="3.12.0",
     license="MIT",
     keywords="chess stockfish",
     python_requires=">=3.6",
