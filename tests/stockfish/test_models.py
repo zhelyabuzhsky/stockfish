c6f2cf75a62906a70093362320b4ad264edefaed
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2021-04-04 19:04:21 +0500

    Improved test

--- tests/stockfish/test_models.py
+++ tests/stockfish/test_models.py
@@ -158,6 +158,7 @@ class TestStockfish:
 
         stockfish.set_elo_rating(1350)
         assert stockfish.get_best_move() in (
+            "b1c3",
             "d2d3",
             "d2d4",
         )
