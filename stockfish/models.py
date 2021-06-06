03184d9881ba0894e353191f7a116fa59415cc15
John Doknjas <jdoknjas@sfu.ca>
2021-06-06 14:25:20 -0700

    Add get_top_moves function.

--- stockfish/models.py
+++ stockfish/models.py
@@ -279,6 +279,56 @@ class Stockfish:
                         }
             elif splitted_text[0] == "bestmove":
                 return evaluation
+    
+    def get_top_moves(self, num_top_moves: int) -> dict:
+        """Returns info on the top moves/PVs in the position.
+        
+        Args:
+            num_top_moves:
+                The number of moves to return info on, assuming there are at least
+                those many legal moves.
+        
+        Returns:
+            A dictionary where the keys are the PV number, and the values
+            are sub-dictionaries with keys for Move, Centipawn, and Mate. The corresponding
+            value for either Centipawn or Mate will be None.
+            If there are no moves in the position, None is returned.
+        """
+        
+        if num_top_moves > self._parameters["MultiPV"] or num_top_moves <= 0:
+            raise ValueError('bad value for num_top_moves')
+        self._go()
+        lines = []
+        while True:
+            text = self._read_line()
+            splitted_text = text.split(" ")
+            lines.append(splitted_text)
+            if splitted_text[0] == "bestmove":
+                break
+        first_moves_of_PVs = {}
+        multiplier = 1 if ("w" in self.get_fen_position()) else -1
+        for current_line in reversed(lines):
+            if current_line[0] == "bestmove":
+                if current_line[1] == "(none)":
+                    return None
+            elif (("multipv" in current_line) and ("depth" in current_line) and 
+                  current_line[current_line.index("depth") + 1] == self.depth):
+                multiPV_number = current_line[current_line.index("multipv") + 1]
+                if int(multiPV_number) <= num_top_moves:
+                    has_centipawn_value = ("cp" in current_line)
+                    has_mate_value = ("mate" in current_line)
+                    if has_centipawn_value == has_mate_value:
+                        raise RuntimeError("Having a centipawn value and mate value should be mutually exclusive.")
+                    first_moves_of_PVs[multiPV_number] = {
+                        "Move": current_line[current_line.index("pv") + 1],
+                        "Centipawn": int(current_line[current_line.index("cp") + 1]) * multiplier if has_centipawn_value else None,
+                        "Mate": int(current_line[current_line.index("mate") + 1]) * multiplier if has_mate_value else None,
+                        "Depth": int(current_line[current_line.index("depth") + 1]),
+                        "Seldepth": int(current_line[current_line.index("seldepth") + 1])
+                    }
+            else:
+                return first_moves_of_PVs
+        raise RuntimeError("Reached the end of get_top_moves without returning anything.")
 
     def set_depth(self, depth_value: int = 2) -> None:
         """Sets current depth of stockfish engine.
