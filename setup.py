20affa6ccf43f5c618bd780202ae9375281ca6f3
Ilya Zhelyabuzhsky <zhelyabuzhsky@icloud.com>
2021-06-15 09:10:48 +0500

    Version 3.17.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="stockfish",
     author="Ilya Zhelyabuzhsky",
     author_email="zhelyabuzhsky@icloud.com",
-    version="3.16.0",
+    version="3.17.0",
     license="MIT",
     keywords="chess stockfish",
     python_requires=">=3.6",
