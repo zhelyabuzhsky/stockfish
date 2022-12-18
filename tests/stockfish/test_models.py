743a3aa434e0bca7efbda789755f3c698aa35cb3
Panagiotis Iatrou <iatrou.panag@gmail.com>
2022-12-18 13:01:14 +0200

    Reformatted test_models.py

--- tests/stockfish/test_models.py
+++ tests/stockfish/test_models.py
@@ -885,9 +885,8 @@ class TestStockfish:
         # involve a king being attacked while it's the opponent's turn.
         old_del_counter = Stockfish._del_counter
         assert Stockfish._is_fen_syntax_valid(fen)
-        if (
-            fen == "8/8/8/3k4/3K4/8/8/8 b - - 0 1"
-            and not (10 < stockfish.get_stockfish_major_version() < 15)
+        if fen == "8/8/8/3k4/3K4/8/8/8 b - - 0 1" and not (
+            10 < stockfish.get_stockfish_major_version() < 15
         ):
             # Since for that FEN, SF 15+ and 10- actually output
             # the best move without crashing (unlike SFs 11 to 14).
